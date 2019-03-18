a7389e9ea908994531f874eb60888c2fff495e44
Samuel Turcotte <sturcot3@confederationcollege.ca>
2019-03-18 16:26:04 -0400

    added updateHealth method

--- OOP2_Major_mockup_PRJ/OutputController.cs
+++ OOP2_Major_mockup_PRJ/OutputController.cs
@@ -8,6 +8,19 @@ namespace OOP2_Major_mockup_PRJ
 {
     class OutputController:Controller
     {
+        //This lets us assign the return value of the function to the text property of the form element. 
+        //takes in a reference to the player object's health field, and updates it with the change.
+        //negative change is damage, positive change is healing.
+        public static string updateHealth(ref int playerHealth, int change)
+        {
+            int newHealth = playerHealth - change;
+            string output = string.Empty;
+            for (int i = 0; i < newHealth; i++)
+            {
+                output += "+ ";
+            }
+            return output;
+        }
 
     }
 }
