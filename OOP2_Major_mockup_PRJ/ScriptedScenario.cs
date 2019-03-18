ba7ab57c54f41fa74795f37561c6666531cb323f
Samuel Turcotte <sturcot3@confederationcollege.ca>
2019-03-18 16:10:23 -0400

    removed temp image testing, added single comment in scriptedScenario

--- OOP2_Major_mockup_PRJ/ScriptedScenario.cs
+++ OOP2_Major_mockup_PRJ/ScriptedScenario.cs
@@ -9,6 +9,8 @@ namespace OOP2_Major_mockup_PRJ
     class ScriptedScenario:Scenario
     {
         //We might need to put the arrays of random pieces in a separate class so this class doesn't inherit all that data for nothing.
+        //I agree that scripted scenario shouldnt inherit all the data from random scenario. 
+        //either we can make a 'Data' class of some sort, or just eliminate the inheritance relationship?
 
         //Scripted Scenario Generation
         public override void GenerateScenario()
