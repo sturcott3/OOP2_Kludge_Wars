ef1279f2083189b2c6712a8e140af8180029b633
Samuel Turcotte <sturcot3@confederationcollege.ca>
2019-03-19 17:31:25 -0400

    created a new branch to mess around with

--- OOP2_Major_mockup_PRJ/ScriptedScenario.cs
+++ OOP2_Major_mockup_PRJ/ScriptedScenario.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace OOP2_Major_mockup_PRJ
 {
     class ScriptedScenario:Scenario
     {
         //We might need to put the arrays of random pieces in a separate class so this class doesn't inherit all that data for nothing.
         //I agree that scripted scenario shouldnt inherit all the data from random scenario. 
-        //either we can make a 'Data' class of some sort, or just eliminate the inheritance relationship?
+        //we should either just make our scripted scenarios have all the same data members, 
+        //or eliminate the inheritance relationship altogether.
 
         //Scripted Scenario Generation
         public override void GenerateScenario()
         {
+
+            //readFile()?;
+
             //Fill fields
         }
     }
